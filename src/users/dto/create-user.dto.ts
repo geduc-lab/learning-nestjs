9c488b1b806c417ba9e4e2c48da58f819db5939a
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-01-27 18:15:49 -0300

    add validation in createUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   password: string;
 }
